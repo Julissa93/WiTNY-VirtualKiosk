87e55adde6430f53ff2cf2150b37cd0afbcbd56a
alfonso222 <alfonsorayo222@gmail.com>
2017-08-02 13:57:44 -0400

    stuff

--- scripts.js
+++ scripts.js
@@ -2,18 +2,38 @@
 
 $(document).ready(function(){
 	$("#blogpage").toggle()
+	$("#announcements").toggle()
+	$("#suggestions").toggle()
 });
 
-
+// blog
 $(document).ready(function(){
 	$("#blogbutton").click(function(){
 	$("#mainpage").fadeOut("fast");
 	$("#blogpage").toggle()
 	$("#blogpage").fadeIn("fast");
-
 	});
 });
 
+// announcements
+$(document).ready(function(){
+	$("#announcementsbutton").click(function(){
+	$("#mainpage").fadeOut("fast");
+	$("#announcements").toggle()
+	$("#announcements").fadeIn("fast");
+	});
+});
+
+// suggestions
+$(document).ready(function(){
+	$("#suggestionsbutton").click(function(){
+	$("#mainpage").fadeOut("fast");
+	$("#suggestions").toggle()
+	$("#suggestions").fadeIn("fast");
+	});
+});
+
+// back1
 $(document).ready(function(){
 	$("#backtomain").click(function(){
 	$("#blogpage").fadeOut("fast");
@@ -22,4 +42,25 @@ $(document).ready(function(){
 
 	});
 });
+
+// back2
+$(document).ready(function(){
+	$("#backtomain2").click(function(){
+	$("#announcements").fadeOut("fast");
+	$("#announcements").toggle()
+	$("#mainpage").fadeIn("fast");
+
+	});
+});
+
+// back3
+$(document).ready(function(){
+	$("#backtomain3").click(function(){
+	$("#suggestions").fadeOut("fast");
+	$("#suggestions").toggle()
+	$("#mainpage").fadeIn("fast");
+
+	});
+});
+
 
